46da4e322a98d1e233136acdf9335e4ad69f67df
kiszner.laszlo <kiszner.laszlo@gmail.com>
2024-02-21 15:42:17 +0100

    added general logging

--- src/main/java/com/home/profile/student/StudentController.java
+++ src/main/java/com/home/profile/student/StudentController.java
@@ -3,9 +3,13 @@ package com.home.profile.student;
 import com.home.profile.student.dto.StudentFullResponseDTO;
 import com.home.profile.student.dto.StudentRequestDTO;
 import com.home.profile.student.dto.StudentResponseDTO;
+import com.home.profile.util.Constants;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +18,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class StudentController {
 
+  Logger logger = LoggerFactory.getLogger(StudentController.class);
+
   @Autowired StudentService studentService;
 
   @PostMapping("/student")
   public ResponseEntity<?> createStudent(@Valid @RequestBody StudentRequestDTO studentRequestDTO) {
+    if (studentRequestDTO.getId() != null) {
+      logger.error(Constants.ID_ON_CREATION_MESSAGE);
+      return new ResponseEntity<>(Constants.ID_ON_CREATION_MESSAGE, HttpStatus.BAD_REQUEST);
+    }
+
     StudentResponseDTO studentResponseDTO = studentService.createStudent(studentRequestDTO);
     return new ResponseEntity<>(studentResponseDTO, HttpStatus.CREATED);
   }
---
+++ src/main/java/com/home/profile/util/Constants.java
@@ -0,0 +1,7 @@
+package com.home.profile.util;
+
+public class Constants {
+
+    public static final String ID_ON_CREATION_MESSAGE = "On creation, id field should not be sent by client.";
+
+}
