dfc80695d2607f129e89e544e0f2f9db0dfb3ea3
kiszner.laszlo <kiszner.laszlo@gmail.com>
2024-02-21 21:42:13 +0100

    username password configurable

--- src/main/java/com/home/profile/address/AddressService.java
+++ src/main/java/com/home/profile/address/AddressService.java
@@ -2,11 +2,18 @@ package com.home.profile.address;
 
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class AddressService {
+
+  @Value("${microservices.username}")
+  String userName;
+
+  @Value("${microservices.password}")
+  String password;
 
   @Autowired private WebClient webClient;
 
@@ -14,7 +21,7 @@ public class AddressService {
     return webClient
         .get()
         .uri(uriBuilder -> uriBuilder.path("/address").queryParam("userId", id).build())
-        .headers(headers -> headers.setBasicAuth("profileService", "profileServicePass"))
+        .headers(headers -> headers.setBasicAuth(userName, password))
         .retrieve()
         .bodyToMono(AddressDTO.class)
         .block();
